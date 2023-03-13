675214effb710eb3a6dd26eb7aa0e0c71c398a47
Stepan Zaiatc <zaiatc.stepan@gmail.com>
2023-03-12 19:14:48 -0700

    changes to the structure and readme

---
+++ src/basic.py
@@ -0,0 +1,230 @@
+import pandas as pd
+import plotly.express as px
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output
+from dash import Dash, html, dcc, dash_table
+
+
+# Load data
+df = pd.read_csv('/Users/stepan_zaiatc/Desktop/suicideWatch/data/master.csv')
+
+# Create app
+app = Dash(__name__, external_stylesheets=[dbc.themes.YETI])
+
+# Define layout
+app.layout = dbc.Container([
+    
+    dbc.Row([
+        dbc.Col(html.H1('Suicide Rates Dashboard', className='text-center'), width=12)
+    ]),
+    
+    dbc.Row([
+        html.H3('Cross-Country Comparisons'),
+        html.H6('Select 4 countries to compare:'),
+        dbc.Col([
+            html.Label('Country 1', className='font-size: small'),
+            dcc.Dropdown(
+                id='country1-dropdown',
+                options=[{'label': country, 'value': country} for country in df.country.unique()],
+                value='Canada',
+                clearable=False,
+                className='mt-2'
+            )
+        ], width={'size': 3, 'offset': 0}),
+
+        dbc.Col([
+            html.Label('Country 2', className='font-size: small'),
+            dcc.Dropdown(
+                id='country2-dropdown',
+                options=[{'label': country, 'value': country} for country in df.country.unique()],
+                value='Germany',
+                clearable=False,
+                className='mt-2'
+            )
+        ], width={'size': 3, 'offset': 0}),
+
+        dbc.Col([
+            html.Label('Country 3', className='font-size: small'),
+            dcc.Dropdown(
+                id='country3-dropdown',
+                options=[{'label': country, 'value': country} for country in df.country.unique()],
+                value='Russian Federation',
+                clearable=False,
+                className='mt-2'
+            )
+        ], width={'size': 3, 'offset': 0}),
+
+        dbc.Col([
+            html.Label('Country 4', className='font-size: small'),
+            dcc.Dropdown(
+                id='country4-dropdown',
+                options=[{'label': country, 'value': country} for country in df.country.unique()],
+                value='Thailand',
+                clearable=False,
+                className='mt-2'
+            )
+        ], width={'size': 3, 'offset': 0})
+    ], className='mb-4 mt-4'),
+
+    dbc.Row([
+        dbc.Col([
+            html.H6('Select year range:', className='font-weight-bold'),
+            dcc.RangeSlider(
+                id='year-slider',
+                min=df.year.min(),
+                max=df.year.max(),
+                value=[df.year.min(), df.year.max()],
+                marks={str(year): str(year) for year in range(df.year.min(), df.year.max()+1, 2)},
+                className='mt-2'
+            )
+        ], width={'size': 12})
+    ], className='mb-4 mt-4'),
+
+    dbc.Row([
+        dbc.Col([
+            dcc.Store(id='data-store', data=df.to_dict('records')),
+            dcc.Graph(id='results-graph')
+     ], width={'size': 12})
+    ]),
+    
+
+
+
+
+
+    dbc.Row([
+        html.Hr(style={'border-width': '3px'}),
+        html.H3('Distribution of Age Groups for Suicides in Each Country')
+    ]),
+
+    dbc.Row([
+        dbc.Col([
+            dcc.Graph(id='results-pie1')
+        ], width={'size': 3, 'offset': 0}),
+
+        dbc.Col([
+            dcc.Graph(id='results-pie2')
+        ], width={'size': 3, 'offset': 0}),
+
+        dbc.Col([
+            dcc.Graph(id='results-pie3')
+        ], width={'size': 3, 'offset': 0}),
+
+        dbc.Col([
+            dcc.Graph(id='results-pie4')
+        ], width={'size': 3, 'offset': 0})
+    ], className='mb-4 mt-4')
+
+
+], fluid=True)
+
+# Define the callback to update the data store
+@app.callback(
+    Output('data-store', 'data'),
+    [Input('country1-dropdown', 'value'),
+    Input('country2-dropdown', 'value'),
+    Input('country3-dropdown', 'value'),
+    Input('country4-dropdown', 'value'),
+    Input('year-slider', 'value')]
+)
+
+def update_data_store(selected_country1, selected_country2, selected_country3, selected_country4, selected_year_range):
+    filtered_df1 = df[(df['country'] == selected_country1) & (df['year'].between(selected_year_range[0], selected_year_range[1]))]
+    filtered_df2 = df[(df['country'] == selected_country2) & (df['year'].between(selected_year_range[0], selected_year_range[1]))]
+    filtered_df3 = df[(df['country'] == selected_country3) & (df['year'].between(selected_year_range[0], selected_year_range[1]))]
+    filtered_df4 = df[(df['country'] == selected_country4) & (df['year'].between(selected_year_range[0], selected_year_range[1]))]
+    
+    df_combined = pd.concat([filtered_df1, filtered_df2, filtered_df3, filtered_df4])
+    return df_combined.to_dict('records')
+
+# Define the callback to update the graphs
+@app.callback(
+    [Output('results-graph', 'figure'),
+    Output('results-pie1', 'figure'),
+    Output('results-pie2', 'figure'),
+    Output('results-pie3', 'figure'),
+    Output('results-pie4', 'figure')
+    ],
+    Input('data-store', 'data')
+)
+
+def update_graphs(data):
+    figures = []
+    df = pd.DataFrame.from_dict(data)
+
+    # Histogram for Suicide Comparisons Between Countries
+
+    fig = px.histogram(df, x='year', y='suicides_100k_pop', color='country', barmode='group')
+    
+    fig.update_layout( 
+        xaxis = dict(
+            title = 'Year',
+            dtick = 1,              # set the interval between x-axis labels
+            tickangle = -60         # set the angle to x-axis labels
+        ),
+        yaxis_title = 'Number of suicides per 100K people',
+        legend = dict(
+            title = "", 
+            orientation = "h",      # set the orientation to 'h'orizontal
+            x = 0.5, 
+            y = -0.4, 
+            xanchor = "center", 
+            yanchor = "bottom"
+        ),
+        title = {
+            'text': 'Suicide Comparisons Between Countries',
+            'x': 0.5,
+            'y': 0.92,
+            'xanchor': 'center',
+            'yanchor': 'top'
+        }
+    )
+
+    # Add the histogram chart to the list of figures
+    figures.append(fig)
+
+
+    # Update pie charts
+
+    legend_order = ['5-14 years', '15-24 years', '25-34 years', '35-54 years', '55-74 years', '75+ years']
+
+    # create a list of unique country names in the dataframe
+    country_list = df['country'].unique()
+
+    # loop through each country and create a pie chart
+    for country in country_list:
+        #hover_data = {'suicides_100k_pop': ':.2f', 'age': True}
+        
+        chart_data = df[df['country'] == country]
+        chart_data['age'] = pd.Categorical(chart_data['age'], categories=legend_order, ordered=True)  # add ordered categories to age column
+        chart_data = chart_data.sort_values('age')  # sort by age column
+        fig = px.pie(chart_data, values='suicides_100k_pop', names='age',
+            labels={'suicides_100k_pop': 'Suicides per 100K Population', 'age': 'Age Group'},
+            title=country)
+        fig.update_traces(
+            textposition='inside',               # set text position to inside of the slices
+            sort=False
+        )
+        fig.update_layout(
+            margin=dict(l=20, r=0, t=30, b=0),  # adjust margin to move the chart position
+            #showlegend = False,
+            legend=dict(
+                orientation='h'                 # horizontal orientatio,          
+            )
+        )
+
+        # Only show the legend for the last pie chart
+        if (country_list[len(country_list)-1] == country):
+            fig.update_layout(showlegend = True)
+
+    
+        # Add the pie chart to the list of figures
+        figures.append(fig)
+
+    return figures
+
+
+
+# Run app
+if __name__ == '__main__':
+    app.run_server(debug=True)
